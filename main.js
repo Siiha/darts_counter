9c2ea35d72c5229b13f4e6acbe991e722f68783d
Siina <siina.hakala@edu.turkuamk.fi>
2025-12-08 20:32:31 +0200

    fix: update player initialization and score update functionality

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import { player } from "./player.js";
 import { mode_301, mode_501 } from "./game_modes.js";
-const players = [player("p1"), player("p2")];
+const players = [new player("player_1"), new player("player_2")];
+let s = 0;
+
+function scoreupdate() {
+    const a = document.getElementById("score");
+    players[s].add(parseInt(a.value));
+    const d = document.getElementById(players[s].name + "_score");
+    d.innerHTML = players[s].scoreboard(301)[0];
+    s = s === 0 ? 1 : 0;
+
+
+}
+const dd = document.getElementById("add");
+form.addEventListener("click", scoreupdate);
