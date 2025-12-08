08a1680fbfc8fb8170245ec8f5eba2247440c2de
paukkusavu <uunikakku@gmail.com>
2025-12-08 13:26:40 +0200

    Update game_modes.js

--- game_modes.js
+++ game_modes.js
@@ -1,4 +1,35 @@
-const p = "Perfect round!";
-export const mode_301 = { startingScore: 301, specialMessages: { 151: "1/2 to finish!", 180: p } };
-export const mode_501 = { startingScore: 501, specialMessages: { 251: "1/2 to finish!", 180: p } };
-export const customMode = (startingScore) => ({ startingScore, specialMessages: { 180: p } });
+export class Player {
+  constructor(name, startingScore) {
+    this.name = name;
+    this.startingScore = startingScore;
+    this.points = []; 
+  }
+
+  addThrow(score) {
+    this.points.push(score);
+  }
+
+  undo() {
+    return this.points.pop();
+  }
+
+  // yhteenlasketut pisteet
+  totalScored() {
+    return this.points.reduce((a, b) => a + b, 0);
+  }
+
+  // jäljellä oleva pistemäärä (301/501 - heitetyt)
+  remaining() {
+    return this.startingScore - this.totalScored();
+  }
+
+  // heittojen keskiarvo (reaaliaikainen aggregointi)
+  average() {
+    if (this.points.length === 0) return 0;
+    return Math.round((this.totalScored() / this.points.length) * 10) / 10;
+  }
+
+  reset() {
+    this.points = [];
+  }
+}
