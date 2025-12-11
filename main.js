bec017de661e09aae00bc5e35ceac6efca50be36
Siina <siina.hakala@edu.turkuamk.fi>
2025-12-11 18:33:46 +0200

    fix: update scoreupdate function to display winner message

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ import { player } from "./player.js";
 import { mode_301, mode_501 } from "./game_modes.js";
 const players = [new player("player_1"), new player("player_2")];
 let s = 0;
+const mes = document.getElementById("message");
 
 function scoreupdate() {
     const a = document.getElementById("score");
     players[s].add(parseInt(a.value));
     const d = document.getElementById(players[s].name + "_score");
     d.innerHTML = players[s].scoreboard(301)[0];
+    if (players[s].scoreboard(301)[0] == 0) {
+        mes.innerHTML = players[s].name + " win!";
+    }
     s = s === 0 ? 1 : 0;
 
 
