f553e5142f8877479e68bd790f82dfd3d4204549
Siina <siina.hakala@edu.turkuamk.fi>
2025-12-14 21:41:40 +0200

    fix: remove game_modes.js file and its associated exports

--- game_modes.js
+++
@@ -1,4 +0,0 @@
-const p = "Perfect round!";
-export const mode_301 = { startingScore: 301, specialMessages: { 151: "1/2 to finish!", 180: p } };
-export const mode_501 = { startingScore: 501, specialMessages: { 251: "1/2 to finish!", 180: p } };
-export const customMode = (startingScore) => ({ startingScore, specialMessages: { 180: p } });
